1a83f84c6c3d8405f90d164e20fc350f0ba68ef4
alexiisv <alexisv@udenar.edu.co>
2024-06-24 16:20:03 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+// scripts.js
+
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+    const showRegister = document.getElementById('show-register');
+    const showLogin = document.getElementById('show-login');
+
+    showRegister.addEventListener('click', (e) => {
+        e.preventDefault();
+        loginForm.classList.add('hidden');
+        registerForm.classList.remove('hidden');
+    });
+
+    showLogin.addEventListener('click', (e) => {
+        e.preventDefault();
+        registerForm.classList.add('hidden');
+        loginForm.classList.remove('hidden');
+    });
+});
+
+function login(event) {
+    event.preventDefault();
+    // Validar el login si es necesario
+    window.location.href = 'music.html'; // Redirige a la nueva página
+}
